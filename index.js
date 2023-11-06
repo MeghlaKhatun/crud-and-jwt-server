849968af4bd880410e15b1101a44a1f059ea057f
meghla khatun <meghlaparvin45@gmail.com>
2023-11-07 00:32:09 +0600

    added a single get api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,16 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result)
     })
+
+    app.get("/service/:id", async(req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id:new ObjectId(id) };
+      const result = await homeServiceCollection.findOne(query);
+      console.log(result)
+      res.send(result)
+    })
+
 
       app.post('/service',async(req,res)=>{
         const addService=req.body;
